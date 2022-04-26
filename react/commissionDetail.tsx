fd97787d956adeca4b27928b00ce47b95a8df0b0
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-04-26 13:16:39 +0200

    feat: import from marketplace

---
+++ react/commissionDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { commissionReportDetail as Detail } from 'vtex.marketplace-financial-commission'
+
+function CommissionDetail() {
+  return (
+    <div>
+      <Detail />
+    </div>
+  )
+}
+
+export default CommissionDetail
