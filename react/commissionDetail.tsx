9c8177ccd579bddb4aa85475fbb1ded6ec9f8176
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-05-12 09:26:10 +0200

    feat: account prop

--- react/commissionDetail.tsx
+++ react/commissionDetail.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import { useRuntime } from 'vtex.render-runtime'
 import { commissionReportDetail as Detail } from 'vtex.marketplace-financial-commission'
 
 function CommissionDetail() {
-  return (
-    <div>
-      <Detail />
-    </div>
-  )
+  const { account } = useRuntime()
+
+  return <Detail seller={account} />
 }
 
 export default CommissionDetail
