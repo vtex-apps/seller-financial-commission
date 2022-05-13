0b5a62be6fe4176524857ca97739d670a4f8c22a
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-05-13 16:21:32 +0200

    fix: route label

--- react/commissionDetail.tsx
+++ react/commissionDetail.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useRuntime } from 'vtex.render-runtime'
 import { commissionReportDetail as Detail } from 'vtex.marketplace-financial-commission'
 
 import SELLER_ORDERS from './graphql/sellerOrders.gql'
 
 function CommissionDetail() {
-  const { account, setQuery } = useRuntime()
-
-  useEffect(() => {
-    setQuery({ sellerName: account })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const { account } = useRuntime()
 
   return <Detail account={account} ordersQuery={SELLER_ORDERS} />
 }
